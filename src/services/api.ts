90edbd8a0542703105ef7b749ca5e20a30b2ffda
Bradley Pizzimenti <bradley@gennakersystems.com>
2023-05-05 22:53:04 -0800

    add api to bring data in from google Apps Script

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
-const API_URL = 'https://t.ly/-qU-'; // Replace with the URL from Google Apps Script deployment
+const API_URL = 
+'https://script.google.com/macros/s/AKfycbxY6NxQvv_LVvaQdMK4nSqcMVM4cpHLxidhvbD4NRPT6dzDyy10HmImaXB-GG8EosLKlg/exec'
 
 export async function fetchPlantings() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
+  const response = await fetch(`${API_URL}?callback=processData`);
+  const text = await response.text();
+  const processData = (data: any) => data;
+  const data = eval(text);
   return data;
 }
+
