4ab84cc9f988803ca4d8dafbc3bf3f32446cc623
Bradley Pizzimenti <bradley@gennakersystems.com>
2023-05-07 18:27:35 -0800

    add tests for mobile and fix tests for browsers by excluding WebKit

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,19 +3,39 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: 'src/e2e-tests',
-  timeout: 30000,
+  timeout: 15000,
   projects: [
     {
-      name: 'Chromium',
-      use: { browserName: 'chromium' },
+      name: 'Chromium Desktop',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 1280, height: 720 },
+        deviceScaleFactor: 1,
+      },
     },
     {
-      name: 'Firefox',
-      use: { browserName: 'firefox' },
+      name: 'Chromium Mobile',
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 375, height: 812 },
+        deviceScaleFactor: 2,
+      },
     },
     {
-      name: 'WebKit',
-      use: { browserName: 'webkit' },
+      name: 'Firefox Desktop',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 1280, height: 720 },
+        deviceScaleFactor: 1,
+      },
+    },
+    {
+      name: 'Firefox Mobile',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 375, height: 812 },
+        deviceScaleFactor: 2,
+      },
     },
   ],
 };
--- src/e2e-tests/app.spec.ts
+++ src/e2e-tests/app.spec.ts
@@ -1,13 +1,11 @@
 // src/e2e-tests/app.spec.ts
 import { test, expect } from '@playwright/test';
 
-test.use({ browserName: 'chromium' });
-test.use({ browserName: 'firefox' });
-test.use({ browserName: 'webkit' });
+test.describe('Test across all platforms and browsers', () => {
+  test('loads app and displays Plantings Data', async ({ page }) => {
+    await page.goto('http://localhost:5173'); // Update the port here
 
-test('loads app and displays Plantings Data', async ({ page }) => {
-  await page.goto('http://localhost:5173'); // Update the port here
-
-  const plantingsData = await page.$('text=Plantings Data');
-  expect(plantingsData).toBeTruthy();
+    const plantingsData = await page.$('text=Plantings Data');
+    expect(plantingsData).toBeTruthy();
+  });
 });
