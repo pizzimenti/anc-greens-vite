9789950b09d3a8736cb1f508242c367b3a3f7a1f
Bradley Pizzimenti <bradley@gennakersystems.com>
2023-05-07 17:38:37 -0800

    update tests to cover all browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,20 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: 'src/e2e-tests',
   timeout: 30000,
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
 };
 
 export default config;
--- src/e2e-tests/app.spec.ts
+++ src/e2e-tests/app.spec.ts
@@ -1,6 +1,10 @@
 // src/e2e-tests/app.spec.ts
 import { test, expect } from '@playwright/test';
 
+test.use({ browserName: 'chromium' });
+test.use({ browserName: 'firefox' });
+test.use({ browserName: 'webkit' });
+
 test('loads app and displays Plantings Data', async ({ page }) => {
   await page.goto('http://localhost:5173'); // Update the port here
 
