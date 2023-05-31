5abadfa4716abd95b8dc69cb55a80fdd95634e3d
Bradley Pizzimenti <bradley@gennakersystems.com>
2023-05-30 19:01:42 -0800

    fix scoping of processData var in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,15 @@
 // Path: src/services/api.ts
 import { Planting } from '../types';
 
-const API_URL = 
-'https://script.google.com/macros/s/AKfycbxY6NxQvv_LVvaQdMK4nSqcMVM4cpHLxidhvbD4NRPT6dzDyy10HmImaXB-GG8EosLKlg/exec'
+const API_URL = 'https://script.google.com/macros/s/AKfycbxY6NxQvv_LVvaQdMK4nSqcMVM4cpHLxidhvbD4NRPT6dzDyy10HmImaXB-GG8EosLKlg/exec';
+
+const processData = (data: any) => data;
 
 export async function fetchPlantings(): Promise<Planting[]> {
   const response = await fetch(`${API_URL}?callback=processData`);
   const text = await response.text();
-  const processData = (data: any) => data;
   const data: Planting[] = eval(text);
   return data;
 }
+
+export { processData };
